8fcaff0a98fc4cf338dffcdd093fcf72eb0e4020
unknown <vanzandt.school.email@gmail.com>
2023-12-21 12:07:08 -0500

    The color change per hour is now fully working, upon hitting a new hour, the color of the background will change accordingly

--- assets/js/script.js
+++ assets/js/script.js
@@ -160,18 +160,94 @@ var currentHour = dayjs();
 
 function updateTime() {
     var currentDay = dayjs().format('MMM DD, YYYY');
-    var currentTime = dayjs().format("hh:mm:ss A");
+    var currentTime = dayjs().format("hh:mm:ss");
     $("#currentDay").text(currentDay + " at " + currentTime);
 }
 
 setInterval(updateTime, 1);
 
-var currentHour = dayjs().hour();
+
 var timeBlocks = document.querySelectorAll(".time-block");
-// console.log(timeBlocks);
+console.log(timeBlocks);
+
+timeBlock = document.getElementById("hour-9");
+timeBlock.setAttribute("data-hour", "9");
+
+//9AM
+
+var timeBlock = $("#time-block");
+timeBlock.data("hour", 9);
+
+//10AM
+
+timeBlock = document.getElementById("hour-10");
+timeBlock.setAttribute("data-hour", "10");
+
+var timeBlock = $("#time-block");
+timeBlock.data("hour", 10);
+
+//11AM
+
+timeBlock = document.getElementById("hour-11");
+timeBlock.setAttribute("data-hour", "11");
+
+var timeBlock = $("#time-block");
+timeBlock.data("hour", 11);
+
+//12PM
+
+timeBlock = document.getElementById("hour-12");
+timeBlock.setAttribute("data-hour", "12");
+
+var timeBlock = $("#time-block");
+timeBlock.data("hour", 12);
+
+//1PM
+
+timeBlock = document.getElementById("hour-13");
+timeBlock.setAttribute("data-hour", "13");
+
+var timeBlock = $("#time-block");
+timeBlock.data("hour", 13);
+
+//2PM
+
+timeBlock = document.getElementById("hour-14");
+timeBlock.setAttribute("data-hour", "14");
+
+var timeBlock = $("#time-block");
+timeBlock.data("hour", 14);
+
+//3PM
+
+timeBlock = document.getElementById("hour-15");
+timeBlock.setAttribute("data-hour", "15");
+
+var timeBlock = $("#time-block");
+timeBlock.data("hour", 15);
+
+//4PM
+
+timeBlock = document.getElementById("hour-15");
+timeBlock.setAttribute("data-hour", "15");
+
+var timeBlock = $("#time-block");
+timeBlock.data("hour", 15);
+
+//5PM
+
+timeBlock = document.getElementById("hour-16");
+timeBlock.setAttribute("data-hour", "16");
+
+var timeBlock = $("#time-block");
+timeBlock.data("hour", 16);
+
+
+var currentHour = dayjs().hour();
 
 timeBlocks.forEach((timeBlock) => {
   var hour = parseInt(timeBlock.getAttribute("data-hour"));
+  console.log(hour);
 
   if (hour < currentHour) {
     timeBlock.classList.add("past");
@@ -181,30 +257,3 @@ timeBlocks.forEach((timeBlock) => {
     timeBlock.classList.add("future");
   }
 });
-
-
-
-
-
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  
-    // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-    // the code isn't run until the browser has finished rendering all the elements
-    // in the html.
